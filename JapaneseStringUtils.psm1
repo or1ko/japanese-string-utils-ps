c46a6b1c44cd698f629b6c9c6220ab83b8f6994c
or1ko <kouji1985@hotmail.com>
2021-04-06 21:29:11 +0900

    ConvertTo-Hankakuを作成

---
+++ JapaneseStringUtils.psm1
@@ -0,0 +1,24 @@
+﻿<#
+ .Synopsis
+  全角文字を半角文字に変換します。
+
+ .Description
+  引数の文字列にある全角文字を半角文字に変換します。
+
+ .Parameter Str
+  変換対象の文字列
+
+ .Example
+   # 全角文字を半角文字に変換 
+   ConvertTo-Ascii "aｂc１23" 
+   abc123
+#>
+function ConvertTo-Hankaku {
+    param([String] $str)
+
+    process {
+      Add-Type -AssemblyName Microsoft.VisualBasic
+      return [Microsoft.VisualBasic.Strings]::StrConv($str, [Microsoft.VisualBasic.VbStrConv]::Narrow)
+    }
+}
+Export-ModuleMember -Function ConvertTo-Hankaku
