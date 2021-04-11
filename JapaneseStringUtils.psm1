ea76c0b0c21f7bba056843eb6a28f07affe32a99
or1ko <kouji1985@hotmail.com>
2021-04-11 22:09:44 +0900

    ConvertFrom-Kansujiを作成

--- JapaneseStringUtils.psm1
+++ JapaneseStringUtils.psm1
@@ -130,17 +130,109 @@ function ConvertFrom-Kansuuji {
         "九" = 9;
       }
 
-
-      $cs = $str.ToCharArray() | 
-      ForEach-Object {
-        if ($kan2ara.ContainsKey("$_")) {
-          $kan2ara["$_"]
-        } else {
-          $_
-        }
+      $tani2ara = @{
+        "十" = 10;
+        "百" = 100;
+        "千" = 1000;
+        "万" = 10000;
+        "億" = [Math]::Pow(10, 8);
+        "兆" = [Math]::Pow(10, 12);
+        "京" = [Math]::Pow(10, 16);
+        "垓" = [Math]::Pow(10, 20);
+        "𥝱" = [Math]::Pow(10, 24);
+        "穣" = [Math]::Pow(10, 28);
+        "溝" = [Math]::Pow(10, 32);
+        "澗" = [Math]::Pow(10, 36);
+        "正" = [Math]::Pow(10, 40);
+        "載" = [Math]::Pow(10, 44);
+        "極"  = [Math]::Pow(10, 48);
+        "恒河沙" = [Math]::Pow(10, 52);
+        "阿僧祇" = [Math]::Pow(10, 56);
+        "那由他" = [Math]::Pow(10, 60);
+        "不可思議" = [Math]::Pow(10, 64);
+        "無量大数" = [Math]::Pow(10, 68);
       }
 
-      $cs -join ""
+      $result = ""
+      $isNum = $false
+
+      $i = 0
+      $beforeNum = $null
+
+      $numResult = 0
+      while($i -lt $str.Length) {
+        $sub = $str.Substring($i);
+
+        $kan2 = $kan2ara.Keys | Where-Object { $sub.StartsWith($_) }
+        if ($kan2.Count) {
+          $isNum = $true
+
+          $beforeNum = $kan2ara[$kan2 + ""]
+          $i = $i + 1
+          continue
+        }
+
+        $matchedTani = $tani2ara.Keys | Where-Object { $sub.StartsWith($_) }
+        if ($matchedTani.Count) {
+          $isNum = $true
+
+          if ($null -eq $beforeNum) {
+            $baseNum = 1
+          } else {
+            $baseNum = $beforeNum
+          }
+          
+          $tani = $tani2ara[$matchedTani + ""]
+
+          if ($tani -gt $numResult) {
+            if ($null -eq $beforeNum) {
+              if ($numResult -eq 0) {
+                $numResult = 1 * $tani
+              } else {
+                $numResult = $numResult * $tani
+              }
+            } else {
+              $numResult = ($numResult + $baseNum) * $tani
+            }
+          } else {
+            $numResult = $numResult + $baseNum * $tani
+          }
+
+          $beforeNum = $null;
+          $i = $i + $matchedTani.Length
+          continue
+        }
+
+        if ($isNum) {
+          $isNum = $false
+
+          if ($null -ne $beforeNum) {
+            $numResult = $numResult + $beforeNum 
+            $beforeNum = $null
+          }
+
+          $result = $result + $numResult
+          $numResult = 0
+        }
+
+        $result = $result + $sub[0]
+        $i = $i + 1
+      }
+
+
+      if ($isNum) {
+
+        if ($null -ne $beforeNum) {
+          $numResult = $numResult + $beforeNum 
+          $beforeNum = $null
+        }
+
+        $result = $result + $numResult
+        $numResult = 0
+      }
+
+      $result
+
     }
 }
 Export-ModuleMember -Function ConvertFrom-Kansuuji
